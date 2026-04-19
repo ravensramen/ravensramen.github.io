0d96b7057589db7832cbd08442731b4273b8a5c9
ravensramen <ravensramen@gmail.com>
2026-04-19 12:22:06 -0700

    Completed simple website

--- new-site/webpack.config.prod.js
+++ new-site/webpack.config.prod.js
@@ -12,8 +12,10 @@ module.exports = merge(common, {
     new CopyPlugin({
       patterns: [
         { from: 'img', to: 'img' },
+        { from: 'images', to: 'images' },
         { from: 'css', to: 'css' },
         { from: 'js/vendor', to: 'js/vendor' },
+        { from: 'resume.html', to: 'resume.html' },
         { from: 'icon.svg', to: 'icon.svg' },
         { from: 'favicon.ico', to: 'favicon.ico' },
         { from: 'robots.txt', to: 'robots.txt' },
